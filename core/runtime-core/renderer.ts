fad162a68f52b402a87f4b5b464266523aca0fd5
zhangshuo00 <443494891@qq.com>
2024-04-15 15:41:11 +0800

    feat: 实现 provide 和 inject;

--- core/runtime-core/renderer.ts
+++ core/runtime-core/renderer.ts
@@ -2,64 +2,64 @@ import {createComponentInstance, setupComponent} from "./component";
 import {ShapeFlags} from "../shared/shapeFlags";
 import {Fragment, Text} from "./vnode";
 
-export function render(vnode, container) {
+export function render(vnode, container, parentComponent) {
   // patch
-  patch(vnode, container)
+  patch(vnode, container, parentComponent)
 }
 
-function patch(vnode, container) {
+function patch(vnode, container, parentComponent) {
   // 处理组件
   const { type, shapeFlag } = vnode;
 
   switch (type) {
     case Fragment:
-      processFragment(vnode, container);
+      processFragment(vnode, container, parentComponent);
       break;
     case Text:
       processText(vnode, container);
       break;
     default:
       if (shapeFlag & ShapeFlags.ELEMENT) {
-        processElement(vnode, container);
+        processElement(vnode, container, parentComponent);
       } else if (shapeFlag & ShapeFlags.STATEFUL_COMPONENT) {
-        processComponent(vnode, container)
+        processComponent(vnode, container, parentComponent)
       }
       break;
   }
 
 }
 
-function processFragment(vnode: any, container: any) {
-  mountChildren(vnode, container);
+function processFragment(vnode: any, container: any, parentComponent) {
+  mountChildren(vnode, container, parentComponent);
 }
 function processText(vnode: any, container: any) {
   const {children} = vnode;
   const textNode = (vnode.el = document.createTextNode(children));
   container.appendChild(textNode);
 }
-function processComponent(vnode: any, container: any) {
-  mountComponent(vnode, container)
+function processComponent(vnode: any, container: any, parentComponent) {
+  mountComponent(vnode, container, parentComponent)
 }
 
-function mountComponent(initialVNode: any, container: any) {
-  const instance = createComponentInstance(initialVNode)
+function mountComponent(initialVNode: any, container: any, parentComponent) {
+  const instance = createComponentInstance(initialVNode, parentComponent)
 
   setupComponent(instance);
   setupRenderEffect(instance, initialVNode, container);
 }
 
-function processElement(vnode: any, container: any) {
-  mountElement(vnode, container)
+function processElement(vnode: any, container: any, parentComponent) {
+  mountElement(vnode, container, parentComponent);
 }
 
-function mountElement(vnode: any, container: any) {
+function mountElement(vnode: any, container: any, parentComponent) {
   const el = (vnode.el = document.createElement(vnode.type));
   const { children, props, shapeFlag } = vnode;
 
   if (shapeFlag & ShapeFlags.TEXT_CHILDREN) {
     el.textContent = children;
   } else if (shapeFlag & ShapeFlags.ARRAY_CHILDREN) {
-    mountChildren(vnode, el)
+    mountChildren(vnode, el, parentComponent)
   }
 
   for (const key in props) {
@@ -75,16 +75,16 @@ function mountElement(vnode: any, container: any) {
   container.append(el)
 }
 
-function mountChildren(vnode, container) {
+function mountChildren(vnode, container, parentComponent) {
   vnode.children.forEach((v) => {
-    patch(v, container);
+    patch(v, container, parentComponent);
   });
 }
 
 function setupRenderEffect(instance: any, initialVNode, container) {
   const { proxy } = instance;
   const subTree = instance.render.call(proxy);
-  patch(subTree, container)
+  patch(subTree, container, instance)
 
   initialVNode.el = subTree.el;
 }
---
+++ example/apiInject/App.js
@@ -0,0 +1,41 @@
+import { h, provide, inject } from "../../lib/guide-mini-vue.esm.js";
+
+const Provider = {
+  name: "Provider",
+  render() {
+    return h("div", {}, [h("p", {}, "Provider"), h(ProviderTwo)]);
+  },
+  setup() {
+    provide("foo", "fooVal");
+    provide("bar", "barVal");
+  }
+}
+
+const ProviderTwo = {
+  name: "ProviderTwo",
+  render() {
+    return h("div", {}, [h("p", {}, `providerTow foo: ${this.foo}`), h(Consumer)]);
+  },
+  setup() {
+    provide("foo", "fooValTwo");
+    const foo = inject("foo");
+    return {
+      foo
+    };
+  },
+};
+
+const Consumer = {
+  name: "Consumer",
+  setup() {
+    const foo = inject("foo");
+    const bar = inject("bar");
+    return {
+      foo,
+      bar
+    };
+  },
+  render() {
+    return h("div", {}, [h("p", {}, `foo: ${this.foo}, bar: ${this.bar}`)]);
+  }
+}
